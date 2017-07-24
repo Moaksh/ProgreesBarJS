aa4fb9104af205bd3ab6f99a71d11cc42b5c8c57
CarlsKe <fernancarldiaz@outlook.com>
2017-07-23 23:33:57 -0500

    feat: added timer line

--- js/style.js
+++ js/style.js
@@ -1,15 +1,35 @@
 'use strict';
 
 (function initialLine() {
-  const line = document.getElementsByClassName('progress-bar');
-  console.log("demo");
-
+  const line = document.getElementById('progress-bar');
+  console.log(line.style.width);
 
   let doSetTimeout = (i) => {
-    setTimeout(function() { console.log(i); },i * 100);
+    setTimeout(function() {
+      line.style.width = i+"%";
+      switch (i) {
+        case 25:
+          line.style.background = "orange";
+          break;
+        case 50:
+          line.style.background = "yellow";
+          break;
+        case 75:
+          line.style.background = "blue";
+          break;
+        case 100:
+          line.style.background ="rgba(0,0,0,0.0)"
+          line.style.border ="0px";
+          break;
+        default:
+
+      }
+    },i * 100);
   }
 
-  for (var i = 26; i <= 100; ++i)
-    doSetTimeout(i);
+  for (let i = 1; i <= 100; ++i)
+
+     doSetTimeout(i);
+
 
 })();
